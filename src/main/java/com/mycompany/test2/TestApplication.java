6cfac8400a67f4fcdfdd0c4a18307dd55ee840b6
bibenga <{ID}+{username}@users.noreply.github.com>
2023-08-09 21:23:25 +0000

    cleanup code

--- src/main/java/com/mycompany/test2/TestApplication.java
+++ src/main/java/com/mycompany/test2/TestApplication.java
@@ -1,6 +1,5 @@
 package com.mycompany.test2;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
