b91a6da7f0044bbf1ba3e1a421389083515ff9ad
bibenga <{ID}+{username}@users.noreply.github.com>
2023-09-13 19:43:15 +0000

    initial test

---
+++ db/src/test/java/com/github/bibenga/palabras/repositories/LanguageRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.github.bibenga.palabras.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.github.bibenga.palabras.entities.Language;
+
+
+// @SpringBootTest(classes = TestConfiguration.class)
+@DataJpaTest(showSql = false)
+// @Import(TestConfiguration.class)
+@TestPropertySource(locations = {"classpath:application-test.properties"})
+public class LanguageRepositoryTest {
+
+    @Autowired
+    private LanguageRepository languageRepository;
+
+    @Test
+    void testSave() {
+        // test for "play" with test
+        var lang = Language.builder()
+                .setId((byte) 1)
+                .setCode("en")
+                .setName("English")
+                .build();
+        languageRepository.save(lang);
+        assertEquals(languageRepository.count(), 1L);
+    }
+}
---
+++ db/src/test/java/com/github/bibenga/palabras/repositories/TestConfiguration.java
@@ -0,0 +1,13 @@
+package com.github.bibenga.palabras.repositories;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootConfiguration
+@EntityScan({ "com.github.bibenga.palabras.entities" })
+@EnableJpaRepositories(basePackages = { "com.github.bibenga.palabras.repositories" })
+@EnableJpaAuditing
+public class TestConfiguration {
+}
