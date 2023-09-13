ac22af9d1001b6ccfe63cdaefcba1e158d2aa4ba
bibenga <{ID}+{username}@users.noreply.github.com>
2023-09-13 10:34:39 +0000

    update dependency hierarchy and load data app

--- loader/src/main/java/com/github/bibenga/palabras/loader/LoadDataApplication.java
+++ loader/src/main/java/com/github/bibenga/palabras/loader/LoadDataApplication.java
@@ -8,18 +8,26 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import com.github.bibenga.palabras.loader.services.LanguageService;
 
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootApplication
 @ComponentScan({ "com.github.bibenga.palabras.loader.services.impl" })
 @EntityScan({ "com.github.bibenga.palabras.entities" })
 @EnableJpaRepositories(basePackages = { "com.github.bibenga.palabras.repositories" })
 @EnableJpaAuditing
+@Log4j2
 public class LoadDataApplication {
 
     public static void main(String[] args) {
         var context = SpringApplication.run(LoadDataApplication.class, args);
         try {
-            var languageService = context.getBean(LanguageService.class);
-            languageService.addDefaultLanguages();
+            var addLanguages = context.getEnvironment().getProperty(
+                    "com.github.bibenga.palabras.add-languages", boolean.class);
+            log.info("should add languages: {}", addLanguages);
+            if (addLanguages == Boolean.TRUE) {
+                var languageService = context.getBean(LanguageService.class);
+                languageService.addDefaultLanguages();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
