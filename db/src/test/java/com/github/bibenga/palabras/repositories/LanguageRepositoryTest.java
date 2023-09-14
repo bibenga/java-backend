d6a56d24a1ee987b5db9539f90d7b901471bc04e
bibenga <{ID}+{username}@users.noreply.github.com>
2023-09-14 22:09:31 +0000

    update container name

--- db/src/test/java/com/github/bibenga/palabras/repositories/LanguageRepositoryTest.java
+++ db/src/test/java/com/github/bibenga/palabras/repositories/LanguageRepositoryTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
 import com.github.bibenga.palabras.entities.Language;
