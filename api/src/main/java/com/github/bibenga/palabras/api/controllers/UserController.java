9d47ce80e0b791e0a95e5903ac1f4c6a4a30d8ca
bibenga <{ID}+{username}@users.noreply.github.com>
2023-09-24 10:51:30 +0000

    update user controler

--- api/src/main/java/com/github/bibenga/palabras/api/controllers/UserController.java
+++ api/src/main/java/com/github/bibenga/palabras/api/controllers/UserController.java
@@ -5,10 +5,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.extern.log4j.Log4j2;
@@ -21,7 +24,7 @@ import java.security.Principal;
 @Log4j2
 @Transactional(readOnly = true)
 public class UserController {
-    @GetMapping("/current")
+    @GetMapping
     @ResponseBody
     public UserDTO getUser(Principal principal) throws LanguageNotFoundException {
         var userDto = UserDTO.builder()
@@ -32,14 +35,30 @@ public class UserController {
         return userDto;
     }
 
-    @GetMapping
+    // @GetMapping
+    // @ResponseBody
+    // @PreAuthorize("isAuthenticated()")
+    // // @PreAuthorize("hasAuthority('USER')")
+    // public UserDTO getCurrentUser(@NotNull Principal principal) throws
+    // LanguageNotFoundException {
+    // var userDto = UserDTO.builder()
+    // .uid(principal.getName())
+    // .authenticated(true)
+    // .build();
+    // log.info("the user is: {}", userDto);
+    // return userDto;
+    // }
+
+    @PatchMapping
     @ResponseBody
     @PreAuthorize("isAuthenticated()")
     // @PreAuthorize("hasAuthority('USER')")
-    public UserDTO getCurrentUser(@NotNull Principal principal) throws LanguageNotFoundException {
+    public UserDTO patchUser(@RequestBody @Valid UserDTO request, @NotNull Principal principal)
+            throws LanguageNotFoundException {
         var userDto = UserDTO.builder()
                 .uid(principal.getName())
                 .authenticated(true)
+                .foto(request.getFoto())
                 .build();
         log.info("the user is: {}", userDto);
         return userDto;
--- api/src/main/java/com/github/bibenga/palabras/api/controllers/UserDTO.java
+++ api/src/main/java/com/github/bibenga/palabras/api/controllers/UserDTO.java
@@ -2,6 +2,8 @@ package com.github.bibenga.palabras.api.controllers;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,8 +16,12 @@ import lombok.AccessLevel;
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 @Builder
 public class UserDTO implements Serializable {
-    @NotBlank
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String uid;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private boolean authenticated;
+
+    @NotBlank
+    private String foto;
 }
